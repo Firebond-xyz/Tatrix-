92ac6a41320cc7f64e810cf7d69da276f8878c33
0xshikhar <shikhar@iiitmanipur.ac.in>
2023-06-23 01:30:59 +0530

    updated components

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,11 @@
-import styles from "../styles/Home.module.css";
-import InstructionsComponent from "../components/InstructionsComponent";
+// import styles from "../styles/Home.module.css";
+import Header from "../components/Header";
+// import Hero from "../components/Hero";
 
 export default function Home() {
   return (
     <div>
-      <main className={styles.main}>
-        <InstructionsComponent>
-          <h1>hi</h1>
-        </InstructionsComponent>
-        <h1>hi</h1>
-
-      </main>
+        <Header />
     </div>
   );
 }
