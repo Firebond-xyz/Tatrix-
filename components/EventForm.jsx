8157efe6f12e636304fc8337d4c0971fdf819337
0xshikhar <shikhar@iiitmanipur.ac.in>
2023-06-24 01:20:50 +0530

    added event page

---
+++ components/EventForm.jsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect, useRef } from 'react'
+import { BsFillArrowRightCircleFill } from 'react-icons/bs'
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+const Eventform = () => {
+    const [eventName, setEventName] = useState('');
+    const [eventDescription, setEventDescription] = useState('');
+    const [eventImage, setEventImage] = useState('');
+    const [eventTime, setEventTime] = useState('');
+
+    const onFormSubmit = async (event) => {
+        event.preventDefault();
+        setError(false);
+        setSuccess(false);
+        console.log("event", event);
+    }
+
+    const nameHandler = (event) => {
+        setEventName(event.target.value);
+    }
+
+    const descriptionHandler = (event) => {
+        setEventDescription(event.target.value);
+    }
+
+    const imageHandler = (event) => {
+        // do ipfs upload here and set the image url to eventImage
+        setEventImage(event.target.value);
+    }
+
+    const timeHandler = (event) => {
+        setEventTime(event.target.value);
+    }
+
+
+    return (
+        <div>
+            {/* create event form  */}
+            <div className="flex flex-col items-center justify-center">
+                <div className="w-full pt-5 max-w-lg">
+                    <form >
+                        <div className=" flex flex-col text-left mb-6">
+                            <label htmlFor="text" className=" mb-2 text-lg font-medium text-white dark:text-white">Enter Event Name</label>
+                            <input type="text" id="input-name" onChange={nameHandler} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="MegaRun 2 Live Stream" required />
+                        </div>
+                        <div className=" flex flex-col text-left mb-6">
+                            <label htmlFor="text" className=" mb-2 text-lg font-medium text-white dark:text-white">Enter Event Description</label>
+                            <input type="text" id="input-name" onChange={descriptionHandler} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Write about your event details here" required />
+                        </div>
+
+                        <div className=" flex flex-col text-left mb-6">
+                            <label htmlFor="text" className=" mb-2 text-lg font-medium text-white dark:text-white">Upload Event Banner</label>
+                            <input type="file" id="input-name" onChange={imageHandler} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:border-purple-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+                        </div>
+
+                        <div className=" flex flex-col text-left mb-6">
+                            <label htmlFor="text" className=" mb-2 text-lg font-medium text-white dark:text-white">Select Event Time</label>
+                            {/* take input of calander date and time */}
+                            <div className='bg-white  block w-full p-2 rounded-lg'>
+                            <DatePicker
+                                placeholderText='2023-07-01 10:00'
+                                selected={eventTime}
+                                onChange={(date) => setEventTime(date)}
+                                showTimeSelect
+                                timeFormat="HH:mm"
+                                dateFormat="yyyy-MM-dd HH:mm"
+                            />
+                            </div>
+                        </div>
+
+                        <button className="inline-flex p-5 text-sm font-medium text-center text-white bg-black rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            List Your Event
+                            <BsFillArrowRightCircleFill className="ml-3" />
+                        </button>
+
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Eventform
--- pages/event.jsx
+++ pages/event.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Link from 'next/link';
 import { BsFillArrowRightCircleFill } from 'react-icons/bs'
-import NFTform from '../components/NFTform';
+import Eventform from '../components/Eventform';
 
 
-export default function Nft() {
+export default function Event() {
     return (
         <div className='h-screen bg-black'>
             <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-indigo-900 via-gray-900 to-black'>
@@ -14,13 +14,14 @@ export default function Nft() {
                         <div className="text-center align-middle w-[450px] h-[600px] p-8 bg-purple-700 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                             <div className="md:mt-4 md:mb-8 mb-6 md:pb-2 bg-gradient-to-r from-[#fff] via-[#fff]/80 to-[#9d9ea1]/50 bg-clip-text 
 text-transparent xl:text-[4rem] md:text-5xl font-bold font-polySans md:max-w-5xl text-center text-[22px] max-w-[575px]">
-                                Create Your NFT</div>
+                                Create Your Event</div>
                             <p className='text-white pb-5'>
-                                Create your own NFT for creating token gated streaming event. 
-                                Some More text here 
+                                Create your own Event for Game Streaming.
+                                <br />
+                                Some More text here
                             </p>
-                            <Link className="inline-flex justify-center align-middle items-center p-5 text-sm font-medium text-center text-white bg-black rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" href="/event">
-                                    Already Minted Your NFT
+                            <Link href="/explore" className="inline-flex justify-center align-middle items-center p-5 text-sm font-medium text-center text-white bg-black rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >
+                                    Already Created Your Event
                                     <BsFillArrowRightCircleFill className="ml-3" />
                             </Link>
 
@@ -30,7 +31,7 @@ text-transparent xl:text-[4rem] md:text-5xl font-bold font-polySans md:max-w-5xl
 
                     <figure className="flex flex-col items-center justify-center pt-10 ">
                         <div className="text-center align-middle w-[450px] h-[600px] p-8 bg-purple-700 border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                            <NFTform />
+                            <Eventform />
                         </div>
                     </figure>
                 </div>
