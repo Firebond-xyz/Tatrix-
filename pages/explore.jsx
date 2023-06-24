6b5ba95f20c7a2e71081c36a4f8513c84c9f9128
0xshikhar <shikhar@iiitmanipur.ac.in>
2023-06-25 01:24:52 +0530

    explore page with events added

--- pages/explore.jsx
+++ pages/explore.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link';
 import { BsFillArrowRightCircleFill } from 'react-icons/bs'
+import { BiHeart } from 'react-icons/bi'
 
 const explore = () => {
     return (
@@ -12,15 +13,128 @@ const explore = () => {
                                 text-transparent xl:text-[4rem] md:text-5xl font-bold font-polySans md:max-w-5xl text-center text-[22px] max-w-[575px]">
                             Explore</div>
                         <p className='text-white text-lg font-bold pb-5'>
-                            Explore all the Gaming Events here 
+                            Explore all the Gaming Events here
                         </p>
                         <Link href="/explore" className="inline-flex justify-center align-middle items-center p-5 text-lg font-medium text-center text-white bg-black rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >
                             Join Current Live Event
                             <BsFillArrowRightCircleFill className="ml-3" />
                         </Link>
-
                     </div>
                 </figure>
+
+                <div>
+                    <div className=" ">
+                        {/* map nft data here using Event Card component to show multiple events*/}
+                        <div className='flex p-10'>
+                            {/* creating duplicate events */}
+                            <div className="card card-compact mx-5 w-[400px] bg-base-100 shadow-xl">
+                                <figure>
+                                    <img src="https://images.unsplash.com/photo-1553481187-be93c21490a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80" alt="Shoes" />
+                                </figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">God Of War Stream</h2>
+                                    <div className='flex'>
+                                        <p>Watch Finals here</p>
+                                        <div className='justify-end'>
+                                            <BiHeart />
+                                        </div>
+                                    </div>
+                                    <div className="card-actions justify-end">
+                                        {/* <button className="btn btn-primary">Buy Now</button> */}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card card-compact mx-5 w-[400px] bg-base-100 shadow-xl">
+                                <figure>
+                                    <img src="https://images.unsplash.com/photo-1586165368502-1bad197a6461?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2758&q=80" alt="Shoes" />
+                                </figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">MetaChess3 Stream</h2>
+                                    <div className='flex'>
+                                        <p>Watch Finals here</p>
+                                        <div className='justify-end'>
+                                            <BiHeart />
+                                        </div>
+                                    </div>
+                                    <div className="card-actions justify-end">
+                                        {/* <button className="btn btn-primary">Buy Now</button> */}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card card-compact mx-5 w-[400px] bg-base-100 shadow-xl">
+                                <figure>
+                                    <img src="https://images.unsplash.com/photo-1551103782-8ab07afd45c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80" alt="Shoes" />
+                                </figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">Mario Stream</h2>
+                                    <div className='flex'>
+                                        <p>Watch Finals here</p>
+                                        <div className='justify-end'>
+                                            <BiHeart />
+                                        </div>
+                                    </div>
+                                    <div className="card-actions justify-end">
+                                        {/* <button className="btn btn-primary">Buy Now</button> */}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='flex p-10'>
+                            {/* creating duplicate events */}
+                            <div className="card card-compact mx-5 w-[400px] bg-base-100 shadow-xl">
+                                <figure>
+                                    <img src="https://images.unsplash.com/photo-1553481187-be93c21490a9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80" alt="Shoes" />
+                                </figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">God Of War Stream</h2>
+                                    <div className='flex'>
+                                        <p>Watch Finals here</p>
+                                        <div className='justify-end'>
+                                            <BiHeart />
+                                        </div>
+                                    </div>
+                                    <div className="card-actions justify-end">
+                                        {/* <button className="btn btn-primary">Buy Now</button> */}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card card-compact mx-5 w-[400px] bg-base-100 shadow-xl">
+                                <figure>
+                                    <img src="https://images.unsplash.com/photo-1586165368502-1bad197a6461?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2758&q=80" alt="Shoes" />
+                                </figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">MetaChess3 Stream</h2>
+                                    <div className='flex'>
+                                        <p>Watch Finals here</p>
+                                        <div className='justify-end'>
+                                            <BiHeart />
+                                        </div>
+                                    </div>
+                                    <div className="card-actions justify-end">
+                                        {/* <button className="btn btn-primary">Buy Now</button> */}
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card card-compact mx-5 w-[400px] bg-base-100 shadow-xl">
+                                <figure>
+                                    <img src="https://images.unsplash.com/photo-1551103782-8ab07afd45c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80" alt="Shoes" />
+                                </figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">Mario Stream</h2>
+                                    <div className='flex'>
+                                        <p>Watch Finals here</p>
+                                        <div className='justify-end'>
+                                            <BiHeart />
+                                        </div>
+                                    </div>
+                                    <div className="card-actions justify-end">
+                                        {/* <button className="btn btn-primary">Buy Now</button> */}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
